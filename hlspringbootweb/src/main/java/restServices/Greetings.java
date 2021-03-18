1afd6a5af4a9fab19cc627907e8514e8fa60f92f
hpracjos <52195752+hpracjos@users.noreply.github.com>
2021-03-18 12:48:09 +0000

    My first Commit

---
+++ hlspringbootweb/src/main/java/restServices/Greetings.java
@@ -0,0 +1,22 @@
+package restServices;
+
+public class Greetings {
+
+    private final long id;
+    private final String content;
+
+    public Greetings(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+
+}
